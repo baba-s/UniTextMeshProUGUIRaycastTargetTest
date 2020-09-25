7b2afd3a35a01f111ebe7769bb1548fd0552b21c
baba_s <shotababa1206@gmail.com>
2020-09-25 21:36:27 +0900

    first commit

---
+++ Editor/TextMeshProUGUIRaycastTargetTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	internal sealed class TextMeshProUGUIRaycastTargetTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void TextMeshProUGUI_の_Raycast_Target_がオフになっているか()
+		{
+			bool IsValid( GameObject gameObject )
+			{
+				var textMeshProUGUI = gameObject.GetComponent<TextMeshProUGUI>();
+				return textMeshProUGUI == null || !textMeshProUGUI.raycastTarget;
+			}
+
+			AllGameObjectTester.Test( IsValid );
+		}
+	}
+}
